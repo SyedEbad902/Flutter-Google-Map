55a76ee1750772b767b993917a6f9bf7a51853df
SyedEbad902 <ebad3088@gmail.com>
2024-07-31 03:56:22 +0500

    location added

--- lib/Screens/map_screen.dart
+++ lib/Screens/map_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -10,15 +13,87 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   @override
+  void initState() {
+    super.initState();
+    getUserLocation();
+  }
+
+  Location locationController = new Location();
+  final Completer<GoogleMapController> mapController =
+      Completer<GoogleMapController>();
+  LatLng? cPosition = null;
+
+  Future<void> cameraPosition(LatLng position) async {
+    final GoogleMapController controller = await mapController.future;
+    CameraPosition newCameraPos = CameraPosition(target: position, zoom: 13);
+    await controller
+        .animateCamera(CameraUpdate.newCameraPosition(newCameraPos));
+  }
+
+  Future<void> getUserLocation() async {
+    bool serviceEnable;
+    PermissionStatus permissionGranted;
+    serviceEnable = await locationController.serviceEnabled();
+    if (serviceEnable) {
+      serviceEnable = await locationController.requestService();
+    } else {
+      return;
+    }
+    permissionGranted = await locationController.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await locationController.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    locationController.onLocationChanged.listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          cPosition =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          cameraPosition(cPosition!);
+          print(cPosition);
+        });
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    const LatLng myLocation = LatLng(30.3753, 69.3451);
+    const LatLng myLocation = LatLng(24.860966, 66.990501);
+    const LatLng newLocation = LatLng(24.860966, 66.990501);
+    const LatLng sourceLocation = LatLng(24.8773, 67.1591);
     return Scaffold(
       appBar: AppBar(
         title: Text("MAp"),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: myLocation, zoom: 13),
-      ),
+      body: cPosition == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GoogleMap(
+              onMapCreated: ((GoogleMapController controller) =>
+                  mapController.complete(controller)),
+              initialCameraPosition: CameraPosition(
+                target: myLocation,
+                zoom: 13,
+              ),
+              markers: {
+                Marker(
+                    markerId: MarkerId("_myLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: cPosition!),
+                Marker(
+                    markerId: MarkerId("_currentLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: newLocation),
+                Marker(
+                    markerId: MarkerId("_sourceLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: sourceLocation),
+              },
+            ),
     );
   }
 }
